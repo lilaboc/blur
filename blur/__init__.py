6757001c333cc58b2a7c37223395ef52650e4260
Zhou Wei <wei.zhou@edoovo.com>
2024-07-24 14:26:03 +0800

    video

--- blur/__init__.py
+++ blur/__init__.py
@@ -2,12 +2,16 @@ import os.path
 import random
 from io import BytesIO
 from typing import List
+from skimage.filters import gaussian
 
 import math
 import pyperclip
 import win32clipboard
 from PIL import ImageGrab, Image, ImageDraw, ImageEnhance, ImageOps
 import re
+from loguru import logger
+import os
+from moviepy.editor import *
 
 
 # https://medium.com/geekculture/deforming-images-in-python-66e0d0dcb17f
@@ -37,7 +41,7 @@ class WaveDeformer:
         return [t for t in zip(target_grid, source_grid)]
 
 
-def send_to_clipboard(im):
+def send_image_to_clipboard(im):
     output = BytesIO()
     im.convert("RGB").save(output, "BMP")
     data = output.getvalue()[14:]
@@ -48,7 +52,7 @@ def send_to_clipboard(im):
     win32clipboard.CloseClipboard()
 
 
-def process(im):
+def process_image(im):
     try:
         width, height = im.size
         dots_per_pixel = 13
@@ -69,12 +73,15 @@ def process(im):
         for i in range(int(height / dots_per_pixel)):
             # draw.line((0, dots_per_pixel * i, width, dots_per_pixel * i), fill='lightgray' if i % 2 == 0 else 'gray', width=2)
             draw.line((0, dots_per_pixel * i, width, dots_per_pixel * i), fill='gray', width=2)
-
-        send_to_clipboard(im)
+        send_image_to_clipboard(im)
     except Exception as exp:
-        print(exp)
+        logger.exception('error')
         input()
 
+def send_video_to_clipboard(path):
+    cmd = f"powershell Set-Clipboard -Path '{path}'"
+    print(cmd)
+    os.system(cmd)
 
 def process_text1(text):
     # https://www.zhihu.com/question/20428571
@@ -107,21 +114,56 @@ def process_text(text):
         pyperclip.copy(result)
 
 
+def process_video(filepath):
+    video = VideoFileClip(filepath)
+    video_blurred = video.fl_image(_blur)
+    txt_clip = VideoFileClip(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'warning.mp4')
+    # txt_clip.resize(height=video_blurred.h, resample=Image.LANCZOS)
+    # txt_clip.resize(height=video_blurred.h)
+    video_out = concatenate_videoclips([txt_clip, video_blurred, txt_clip], method="compose")
+    # video_out = CompositeVideoClip([video_blurred, txt_clip])
+    # video_out = CompositeVideoClip([txt_clip, video_blurred])
+    filename, file_extension = os.path.splitext(filepath)
+    output_path = filename + "_blurred" + file_extension
+    video_out.write_videofile(output_path)
+    send_video_to_clipboard(output_path)
+
+# copy video file into clipboard using winclipboard32
+
+def _blur(image):
+    """ Returns a blurred (radius=2 pixels) version of the image """
+    return gaussian(image.astype(float), sigma=2)
+
 def main():
     im = ImageGrab.grabclipboard()
     if isinstance(im, Image.Image):
-        process(im)
+        process_image(im)
     elif isinstance(im, List) and isinstance(im[0], str) and os.path.exists(im[0]):
+        filepath = im[0]
+        filename, file_extension = os.path.splitext(filepath)
         try:
-            im = Image.open(im[0])
-            process(im)
+            if file_extension in ['.mp4', '.avi']:
+                process_video(filepath)
+            else:
+                im = Image.open(filepath)
+                process_image(im)
         except Exception as exp:
-            print(exp)
+            logger.exception('error')
             input()
     else:
         random.choice([process_text, process_text1])(pyperclip.paste())
         # process_text(pyperclip.paste())
 
 
+def test():
+    from moviepy.config import change_settings
+    change_settings({"IMAGEMAGICK_BINARY": r"C:\Users\stern\scoop\apps\imagemagick\current\magick.EXE"})
+    print(TextClip.list('font'))
+    txt_clip = (TextClip("这些都是什么?", fontsize=70, color='white', font='华文宋体')
+                .set_position('center')
+                .set_duration(2))
+    txt_clip.write_videofile("warning.mp4", fps=24)
+
+
 if __name__ == '__main__':
-    main()
+    test()
